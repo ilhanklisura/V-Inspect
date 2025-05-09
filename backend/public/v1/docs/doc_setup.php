218d0a175fd6b14fcd76d62b30772cb30d54ed5a
Ilhan Klisura <work@ilhanklisura.com>
2025-05-09 16:24:09 +0200

    docs(api): add Swagger annotations for User routes

---
+++ backend/public/v1/docs/doc_setup.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @OA\OpenApi(
+ *   @OA\Info(
+ *     title="V-Inspect API",
+ *     version="1.0.0",
+ *     description="Official API documentation for the V-Inspect web platform. This API enables full CRUD operations for managing users, inspections, stations, vehicles, and many more."
+ *   ),
+ *   @OA\Server(
+ *     url=LOCALSERVER,
+ *     description="Local API Server"
+ *   ),
+ *  @OA\Server(
+ *    url=PRODSERVER,
+ *    description="Production API Server"
+ *   )
+ * )
+ * @OA\SecurityScheme(
+ *     securityScheme="ApiKey",
+ *     type="apiKey",
+ *     in="header",
+ *     name="Authentication"
+ * )
+ */
